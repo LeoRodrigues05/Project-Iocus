e7e8b1b4caaedac9b19b95324231c2b875fe3bc1
Leo Raphael Rodrigues <69413960+LeoRodrigues05@users.noreply.github.com>
2021-08-22 10:43:14 +0400

    Create keep_alive.py
    Added file that keeps the bot running 24*7 using flask.

---
+++ keep_alive.py
@@ -0,0 +1,16 @@
+#keeps code running 
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Hello. I'm aware!"
+
+def run():
+  app.run(host='0.0.0.0',port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
